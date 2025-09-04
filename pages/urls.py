0f98a7a45cde0fff5dec84c8b75d625602105fcc
dpreciado22 <desipreciado18@gmail.com>
2025-09-04 04:42:41 -0700

    about-us

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,6 @@ app_name = "pages"
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="homepage"),
-    path("about/", AboutUsView.as_view(), name="about"),
+    path("about-us/", AboutUsView.as_view(), name="about-us"),
     path("account/", MyAccountView.as_view(), name="my-account"),
 ]
