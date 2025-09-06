743d546b5f78779173bd5219f4a7a39b329760b7
dpreciado22 <desipreciado18@gmail.com>
2025-09-06 08:07:20 -0700

    User Registration

--- play2learn/urls.py
+++ play2learn/urls.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Admin
     path("admin/doc/", include("django.contrib.admindocs.urls")),
     path("admin/", admin.site.urls),
+
+    # User Management
+    path('account/', include('allauth.urls')),
+
+    # Local Apps
     path("", include("pages.urls")),
     path("", include(("games.urls","games"), namespace="games")),
+
 ]
