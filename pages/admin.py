d020718f9c545d0f00aff415d19ae600b710ee53
dpreciado22 <desipreciado18@gmail.com>
2025-09-05 22:18:23 -0700

    admin

--- pages/admin.py
+++ pages/admin.py
@@ -6,9 +6,13 @@ class GameScoreAdmin(admin.ModelAdmin):
     list_display = ('user', 'game', 'score', 'finished_at')
     list_filter  = ('game', 'finished_at')
     search_fields = ('user__username',)
+    def get_readonly_fields(self, request, obj=None):
+        return ("finished_at",) if obj else ()
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('user', 'title', 'featured', 'created_at')
+    list_display = ('user', 'title', 'featured')
     list_filter  = ('featured', 'created_at')
     search_fields = ('title', 'body', 'user__username')
+    def get_readonly_fields(self, request, obj=None):
+        return ("created_at", "updated_at") if obj else ()
--- play2learn/urls.py
+++ play2learn/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("admin/doc/", include("django.contrib.admindocs.urls")),
     path("admin/", admin.site.urls),
     path("", include("pages.urls")),
     path("", include(("games.urls","games"), namespace="games")),
